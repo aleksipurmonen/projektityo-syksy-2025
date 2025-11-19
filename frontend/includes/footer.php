55ec1cd4d5e5fb22ecccc486bdaa4a67b3f07a6d
NiklasMonkkonen <niklas.monkkonen7@gmail.com>
2025-11-19 13:13:58 +0200

    footer

--- frontend/includes/footer.php
+++ frontend/includes/footer.php
@@ -1,2 +1,1 @@
-<h1 style="position:absolute; bottom:5px; color: black;" >foote©2025 Weather App -  kaikki oikeudet  pidetään
-r</h1>
+<h1 style="position:absolute; bottom:5px; color: black;" >foote©2025 Weather App -  kaikki oikeudet  pidetään</h1>
