50d6994a5c579bdb76818d1e07519acd54c91945
NiklasMonkkonen <niklas.monkkonen7@gmail.com>
2025-11-19 14:31:12 +0200

    Muutettu footer tyyppiä

--- frontend/includes/footer.php
+++ frontend/includes/footer.php
@@ -1,1 +1,1 @@
-<h1 style="position:absolute; bottom:5px; color: black;" >©2025 Weather App -  kaikki oikeudet  pidetään</h1>
+<footer style="position:absolute; bottom:5px; color: black;" >©2025 Weather App -  kaikki oikeudet  pidetään</footer>
