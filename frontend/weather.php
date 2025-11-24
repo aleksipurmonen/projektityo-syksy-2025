02494479b96ba591328112af1bef65bf130d2663
NiklasBoman <nikke.boman@gmail.com>
2025-11-24 13:10:29 +0200

    SÄÄSIVUN ALKUA

--- frontend/weather.php
+++ frontend/weather.php
@@ -7,7 +7,12 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <div class="container2">
+        <h3>Sään seuranta</h3>
+        <p>Tervetuloa sään seurantajärjestelmään!</p>
+        
+        
+    </div>
 </body>
 </html>
 
