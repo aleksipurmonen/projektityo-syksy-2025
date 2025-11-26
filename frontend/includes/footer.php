09746b46ea8f43060d8cf43954132d57ce56ad7c
aleksipurmonen <aleksi.purmonen@edu.sakky.fi>
2025-11-26 08:23:52 +0200

    headerin ja footerin teksti näkyvämpi

--- frontend/includes/footer.php
+++ frontend/includes/footer.php
@@ -1,3 +1,3 @@
 <div class="footer">
-    <p style="color: black;" >©2025 Weather App - kaikki oikeudet pidätetään</p>
+    <p style="color: #a4ddff;text-shadow: 0px 0px 7px black;" >©2025 Weather App - kaikki oikeudet pidätetään</p>
 </div>
