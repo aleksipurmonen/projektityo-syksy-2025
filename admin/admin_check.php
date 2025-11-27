932a6b79eb3ddc06c5f035420c66a1081c514d66
NiklasMonkkonen <niklas.monkkonen7@gmail.com>
2025-11-27 08:51:49 +0200

    korjaus

--- admin/admin_check.php
+++ admin/admin_check.php
@@ -1,5 +1,5 @@
 <?php
-if (session-status() == PHP_SESSION_NONE) {
+if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 if (!isset($_SESSION['is_admin']) || $_SESSION['is_admin'] !== true) {
