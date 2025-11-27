a73d9227fd1187f3c1047618a42391bb1ef4b262
NiklasMonkkonen <niklas.monkkonen7@gmail.com>
2025-11-27 13:19:32 +0200

    korjattu logout nappula

--- frontend/includes/header.php
+++ frontend/includes/header.php
@@ -1,13 +1,18 @@
 <div class="header">
     <img src="../frontend/assets/images/logo_vanha.png" alt="logo" class="logo">
     <?php
-    
-    //Kirjaudu sisään/ulos painike riippuen siitä, onko käyttäjä kirjautunut vai ei
+    // Kirjaudu sisään/ulos painike riippuen siitä, onko käyttäjä kirjautunut vai ei
     session_start();
     if(!isset($_SESSION["userid"])){
-        echo '<a href="login.php" class="header-btn">Kirjaudu sisään</a>';
-    }else{
-        echo '<div style="display: flex; justify-content: space-between;"><p style="margin:0px;padding:10px;color: #a4ddff;text-shadow: 0px 0px 7px black;" class="header-username">Kirjautunut '.$_SESSION["name"].'</p><a href="logout.php" class="header-btn">Kirjaudu ulos</a></div>';
+        // HUOM! lisätty ../frontend/ polku
+        echo '<a href="../frontend/login.php" class="header-btn">Kirjaudu sisään</a>';
+    } else {
+        echo '<div style="display: flex; justify-content: space-between;">
+                <p style="margin:0px;padding:10px;color: #a4ddff;text-shadow: 0px 0px 7px black;" class="header-username">
+                    Kirjautunut '.$_SESSION["name"].'
+                </p>
+                <a href="../frontend/logout.php" class="header-btn">Kirjaudu ulos</a>
+              </div>';
     }
     ?>
 </div>
