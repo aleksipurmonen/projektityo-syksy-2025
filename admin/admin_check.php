a322c5bac86149cca491867d55d67667a78753e9
NiklasMonkkonen <niklas.monkkonen7@gmail.com>
2025-11-27 08:46:58 +0200

    korjattu

--- admin/admin_check.php
+++ admin/admin_check.php
@@ -3,7 +3,7 @@ if (session-status() == PHP_SESSION_NONE) {
     session_start();
 }
 if (!isset($_SESSION['is_admin']) || $_SESSION['is_admin'] !== true) {
-    header('Location: /login.php');
+    header('Location: ../frontend/login.php');
     exit();
 }
 ?>
